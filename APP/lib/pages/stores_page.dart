1e8dbb6536cca815367b98c6e7a766c23330637e
pvl-marci <paulmarciniak2603@gmail.com>
2022-01-12 12:37:34 +0100

    storelocator

--- APP/lib/pages/stores_page.dart
+++ APP/lib/pages/stores_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewExample extends StatefulWidget {
@@ -14,11 +15,54 @@ class WebViewExampleState extends State<WebViewExample> {
     // Enable virtual display.
   }
 
+  openURL() async {
+    const url = 'https://www.google.de/maps/search/bio+company/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: 'https://www.biocompany.de/',
-      javascriptMode: JavascriptMode.unrestricted,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF375D3B),
+                          Color(0xFFC4D7A4),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    openURL();
+                  },
+                  child: const Text('Find Stores'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
